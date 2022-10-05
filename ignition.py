e1fd2c9e4b5398c7f639f5c784f5e6e407ab6e56
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-05 17:18:54 +0100

    10... 9... 8... ...  Lift off!!!!

---
+++ ignition.py
@@ -0,0 +1,11 @@
+#!python3
+
+time_remaining = 10
+
+while time_remaining >= 0:
+    print(str(time_remaining))
+    time_remaining = time_remaining - 1
+
+print("Ignition...")
+print("LIFT OFF!!!!!!")
+
