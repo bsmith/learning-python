8e6072b6ae86ef01ac04e9381536c61fd3bfb267
Benjamin Smith <bsmith@cabbage.org.uk>
2022-10-15 22:27:02 +0100

    Improve ignition.py to pass a `pylint`

--- ignition.py
+++ ignition.py
@@ -1,11 +1,14 @@
 #!python3
+"""Countdown to Liftoff!"""
 
-time_remaining = 10
+def do_countdown():
+    """Print out the countdown"""
+    time_remaining = 10
 
-while time_remaining >= 0:
-    print(str(time_remaining))
-    time_remaining = time_remaining - 1
+    while time_remaining >= 0:
+        print(str(time_remaining))
+        time_remaining = time_remaining - 1
 
+do_countdown()
 print("Ignition...")
 print("LIFT OFF!!!!!!")
-
